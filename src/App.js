dc203b396b661f662f7eb88523243ba9b19c36d0
Mariana Caramori <musardo@mail.com>
2020-09-06 14:26:43 +0100

    removed yarn lock

--- src/App.js
+++ src/App.js
@@ -1,32 +1,17 @@
 import React, {useState} from 'react';
-// import { DnDProvider } from 'react-dnd';
-// import { HTML5Backend } from 'react-dnd-html5-backend';
-
 import GlobalStyle from './styles/global';
 import Header from './components/Header';
 import Board from './components/Board';
-
-
 import {DragDropContext, Droppable, Draggable} from 'react-beautiful-dnd';
-// function App() {
-//   return (
-//     <>
-//       <Header />
-//       <Board />
-//       <GlobalStyle />
-//     </>
-//   );
-// }
 
-
-const App = () => {
+function App() {
   return (
     <>
       <Header />
       <Board />
       <GlobalStyle />
     </>
-  )
+  );
 }
 
 export default App;
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import { Container } from './styles';
 import {DragDropContext, Droppable, Draggable} from 'react-beautiful-dnd';
-// import  { useDrag } from 'react-dnd';
-// import ITEM_TYPE from '../Constants'
+
 
 export default function Card(props) {
-  // const [{ isDragging }, dragRef] = useDrag({
-  //   item: { type: ITEM_TYPE },
-  //   collect: monitor => ({
-  //     isDragging: monitor.isDragging(),
-  //   })
-  // })
+
   return (
     <Draggable draggableId={props.id} index={props.index} index={props.index}>
     {(provided, snapshot) => (
