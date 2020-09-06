506d76c7adce3f21cb5bfac02c7e5e80b26781d4
Mariana Caramori <musardo@mail.com>
2020-09-06 17:30:19 +0100

    more items to db

--- src/index.js
+++ src/index.js
@@ -59,7 +59,7 @@ const initialState = {
       venda: 2900
     },
     {
-      id: 3165,
+      id: 3213,
       name: "Analista Desenvolvedor Java SR",
       logo: "https://picsum.photos/200/300?random=1",
       abertura: "08/10/2020",
@@ -78,7 +78,64 @@ const initialState = {
       venda: 200
     },
     {
-      id: 408,
+      id: 77,
+      name: "Analista Desenvolvedor Java SR",
+      logo: "https://picsum.photos/200/300?random=1",
+      abertura: "08/10/2020",
+      termino: "21/10/2020",
+      vagas: 3,
+      remuneracao: [
+        {
+          tipo: "CLT",
+          valor: 1200.30
+        },
+        {
+          tipo: "Ben",
+          valor: 600.27
+        },
+      ],
+      venda: 100
+    },
+    {
+      id: 23761,
+      name: "Analista Desenvolvedor Java SR",
+      logo: "https://picsum.photos/200/300?random=1",
+      abertura: "08/10/2020",
+      termino: "21/10/2020",
+      vagas: 3,
+      remuneracao: [
+        {
+          tipo: "CLT",
+          valor: 1200.30
+        },
+        {
+          tipo: "Ben",
+          valor: 600.27
+        },
+      ],
+      venda: 100
+    },
+    {
+      id: 9827,
+      name: "Analista Desenvolvedor Java SR",
+      logo: "https://picsum.photos/200/300?random=1",
+      abertura: "08/10/2020",
+      termino: "21/10/2020",
+      vagas: 3,
+      remuneracao: [
+        {
+          tipo: "CLT",
+          valor: 1200.30
+        },
+        {
+          tipo: "Ben",
+          valor: 600.27
+        },
+      ],
+      venda: 100
+    },
+    {
+      id: 90092,
       name: "Analista Desenvolvedor Java SR",
       logo: "https://picsum.photos/200/300?random=1",
       abertura: "08/10/2020",
