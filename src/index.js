c882820d28bd09d38f0cb5dca2f865032947d3e0
Mariana Caramori <musardo@mail.com>
2020-09-06 17:38:39 +0100

    moved temporary db to another file

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { Provider } from 'react-redux';
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import logger from 'redux-logger';
 
+import { initialState } from './temporary_db';
+
 import vagasReducer from './reducers/vagas_reducer';
 import currentTabReducer from './reducers/current_tab_reducer';
 
@@ -17,144 +19,6 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const middlewares = applyMiddleware(logger);
 
-
-const initialState = {
-  vagas: [
-    {
-      id: 1008,
-      name: "Analista Desenvolvedor Java SR",
-      logo: "https://picsum.photos/200/300?random=1",
-      abertura: "08/10/2020",
-      termino: "21/10/2020",
-      vagas: 3,
-      remuneracao: [
-        {
-          tipo: "CLT",
-          valor: 1200.30
-        },
-        {
-          tipo: "Ben",
-          valor: 600.27
-        },
-      ],
-      venda: 900
-    },
-    {
-      id: 2009,
-      name: "Analista Desenvolvedor Java SR",
-      logo: "https://picsum.photos/200/300?random=1",
-      abertura: "08/10/2020",
-      termino: "21/10/2020",
-      vagas: 3,
-      remuneracao: [
-        {
-          tipo: "CLT",
-          valor: 1200.30
-        },
-        {
-          tipo: "Ben",
-          valor: 600.27
-        },
-      ],
-      venda: 2900
-    },
-    {
-      id: 3213,
-      name: "Analista Desenvolvedor Java SR",
-      logo: "https://picsum.photos/200/300?random=1",
-      abertura: "08/10/2020",
-      termino: "21/10/2020",
-      vagas: 3,
-      remuneracao: [
-        {
-          tipo: "CLT",
-          valor: 1200.30
-        },
-        {
-          tipo: "Ben",
-          valor: 600.27
-        },
-      ],
-      venda: 200
-    },
-    {
-      id: 77,
-      name: "Analista Desenvolvedor Java SR",
-      logo: "https://picsum.photos/200/300?random=1",
-      abertura: "08/10/2020",
-      termino: "21/10/2020",
-      vagas: 3,
-      remuneracao: [
-        {
-          tipo: "CLT",
-          valor: 1200.30
-        },
-        {
-          tipo: "Ben",
-          valor: 600.27
-        },
-      ],
-      venda: 100
-    },
-    {
-      id: 23761,
-      name: "Analista Desenvolvedor Java SR",
-      logo: "https://picsum.photos/200/300?random=1",
-      abertura: "08/10/2020",
-      termino: "21/10/2020",
-      vagas: 3,
-      remuneracao: [
-        {
-          tipo: "CLT",
-          valor: 1200.30
-        },
-        {
-          tipo: "Ben",
-          valor: 600.27
-        },
-      ],
-      venda: 100
-    },
-    {
-      id: 9827,
-      name: "Analista Desenvolvedor Java SR",
-      logo: "https://picsum.photos/200/300?random=1",
-      abertura: "08/10/2020",
-      termino: "21/10/2020",
-      vagas: 3,
-      remuneracao: [
-        {
-          tipo: "CLT",
-          valor: 1200.30
-        },
-        {
-          tipo: "Ben",
-          valor: 600.27
-        },
-      ],
-      venda: 100
-    },
-    {
-      id: 90092,
-      name: "Analista Desenvolvedor Java SR",
-      logo: "https://picsum.photos/200/300?random=1",
-      abertura: "08/10/2020",
-      termino: "21/10/2020",
-      vagas: 3,
-      remuneracao: [
-        {
-          tipo: "CLT",
-          valor: 1200.30
-        },
-        {
-          tipo: "Ben",
-          valor: 600.27
-        },
-      ],
-      venda: 100
-    }
-  ]
-}
 
 
 ReactDOM.render(
---
+++ src/temporary_db.js
@@ -0,0 +1,137 @@
+export const initialState = {
+  vagas: [
+    {
+      id: 1008,
+      name: "Analista Desenvolvedor Java SR",
+      logo: "https://picsum.photos/200/300?random=1",
+      abertura: "08/10/2020",
+      termino: "21/10/2020",
+      vagas: 3,
+      remuneracao: [
+        {
+          tipo: "CLT",
+          valor: 1200.30
+        },
+        {
+          tipo: "Ben",
+          valor: 600.27
+        },
+      ],
+      venda: 900
+    },
+    {
+      id: 2009,
+      name: "Analista Desenvolvedor Java SR",
+      logo: "https://picsum.photos/200/300?random=1",
+      abertura: "08/10/2020",
+      termino: "21/10/2020",
+      vagas: 3,
+      remuneracao: [
+        {
+          tipo: "CLT",
+          valor: 1200.30
+        },
+        {
+          tipo: "Ben",
+          valor: 600.27
+        },
+      ],
+      venda: 2900
+    },
+    {
+      id: 3213,
+      name: "Analista Desenvolvedor Java SR",
+      logo: "https://picsum.photos/200/300?random=1",
+      abertura: "08/10/2020",
+      termino: "21/10/2020",
+      vagas: 3,
+      remuneracao: [
+        {
+          tipo: "CLT",
+          valor: 1200.30
+        },
+        {
+          tipo: "Ben",
+          valor: 600.27
+        },
+      ],
+      venda: 200
+    },
+    {
+      id: 77,
+      name: "Analista Desenvolvedor Java SR",
+      logo: "https://picsum.photos/200/300?random=1",
+      abertura: "08/10/2020",
+      termino: "21/10/2020",
+      vagas: 3,
+      remuneracao: [
+        {
+          tipo: "CLT",
+          valor: 1200.30
+        },
+        {
+          tipo: "Ben",
+          valor: 600.27
+        },
+      ],
+      venda: 100
+    },
+    {
+      id: 23761,
+      name: "Analista Desenvolvedor Java SR",
+      logo: "https://picsum.photos/200/300?random=1",
+      abertura: "08/10/2020",
+      termino: "21/10/2020",
+      vagas: 3,
+      remuneracao: [
+        {
+          tipo: "CLT",
+          valor: 1200.30
+        },
+        {
+          tipo: "Ben",
+          valor: 600.27
+        },
+      ],
+      venda: 100
+    },
+    {
+      id: 9827,
+      name: "Analista Desenvolvedor Java SR",
+      logo: "https://picsum.photos/200/300?random=1",
+      abertura: "08/10/2020",
+      termino: "21/10/2020",
+      vagas: 3,
+      remuneracao: [
+        {
+          tipo: "CLT",
+          valor: 1200.30
+        },
+        {
+          tipo: "Ben",
+          valor: 600.27
+        },
+      ],
+      venda: 100
+    },
+    {
+      id: 90092,
+      name: "Analista Desenvolvedor Java SR",
+      logo: "https://picsum.photos/200/300?random=1",
+      abertura: "08/10/2020",
+      termino: "21/10/2020",
+      vagas: 3,
+      remuneracao: [
+        {
+          tipo: "CLT",
+          valor: 1200.30
+        },
+        {
+          tipo: "Ben",
+          valor: 600.27
+        },
+      ],
+      venda: 100
+    }
+  ]
+}
