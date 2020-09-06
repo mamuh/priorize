bb8285b99ab0a0625b812c86530228ba3420bdc0
Mariana Caramori <musardo@mail.com>
2020-09-07 00:21:18 +0100

    footer css set

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,7 +9,10 @@ export default function Footer() {
   return (
     <Container>
       <div className="footer">
-        footer
+        <img src="https://image.flaticon.com/icons/svg/130/130918.svg" alt="vagas"/>
+        <img src="https://image.flaticon.com/icons/svg/2097/2097743.svg" alt=""/>
+        <img src="https://image.flaticon.com/icons/svg/3010/3010912.svg" alt=""/>
+        <img src="https://image.flaticon.com/icons/svg/929/929564.svg" alt=""/>
       </div>
     </Container>
   );
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -2,6 +2,25 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   background: #fff;
+  width: 100%;
   box-shadow: -10px 0 10px rgba(0,0,0,0.25);
-  height: 50px;
+  height: 60px;
+  position: fixed;
+  bottom: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .footer {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 30px;
+  }
+
+  img {
+    width: 35px;
+    height: 35px;
+  }
 `
