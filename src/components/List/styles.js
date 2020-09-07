38ce098506488ec84057014c081e0c0680564161
Mariana Caramori <musardo@mail.com>
2020-09-07 13:48:52 +0100

    removed scroll

--- src/components/List/styles.js
+++ src/components/List/styles.js
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 export const Container = styled.div`
   text-align: center;
   height: 100%;
-  overflow: scroll;
 
   ul:last-child {
     margin-bottom: 50px;
