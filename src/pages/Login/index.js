46d1231771a452e7dd1a879aa2a3a3f4ee09ebeb
Mariana Caramori <musardo@mail.com>
2020-09-17 22:10:25 +0100

    google login fix

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -38,20 +38,30 @@ const Login = ({ history }) => {
   //   firebase.auth().signInWithRedirect(googleAuthProvider);
   // }
 
-  const handleGoogleLogin = useCallback(async event => {
+  const handleGoogleLogin = (event) => {
     event.preventDefault();
     const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
-    console.log("google login")
-    try {
-      handleLoad(true);
-      await firebase.auth().signInWithPopup(googleAuthProvider);
-      handleLoad(false);
-      history.push("/vagas");
-    } catch(error) {
-      handleFailure(true)
+
+    firebase.auth().signInWithPopup(googleAuthProvider).then(function(result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      // ...
+    }).catch(function(error) {
       console.log(error)
-    }
-  }, [history]);
+    });
+  }
+
+
+
+
+
+  //   firebase.auth().signInWithPopup(googleAuthProvider).then(function(result) {
+  //     console.log(result)
+  //     console.log("google login")
+  //   )}
+  // }, [history]);
 
   // let history = useHistory()
 
